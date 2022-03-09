9f5bb5b8d6ab29d6974cf6dec6f76dcc61524f2e
rpashev <rossen1991@gmail.com>
2022-03-09 17:48:28 +0200

    huddle landing page done

--- src/pages/huddle-landing-page/index.js
+++ src/pages/huddle-landing-page/index.js
@@ -1,9 +1,37 @@
 import styles from "./index.module.scss";
+import logo from "../../assets/huddle-landing-page-with-single-introductory-section-master/images/logo.svg";
+import dashboard from "../../assets/huddle-landing-page-with-single-introductory-section-master/images/illustration-mockups.svg";
+import fb from "../../assets/huddle-landing-page-with-single-introductory-section-master/images/icon-facebook.svg";
+import twitter from "../../assets/huddle-landing-page-with-single-introductory-section-master/images/icon-twitter.svg";
+import pinterest from "../../assets/huddle-landing-page-with-single-introductory-section-master/images/icon-pinterest.svg";
 
 const HuddleLandingPage = () => {
   return (
     <div className={styles.page}>
-      <div className={styles.card}>HI</div>
+      <img src={logo} className={styles.logo} alt="logo"></img>
+      <div className={styles.content}>
+        <img
+          src={dashboard}
+          alt="desktop dashboard"
+          className={styles.dashboard}
+        ></img>
+        <div className={styles.prompt}>
+          <h1>
+            Build The Community<br></br> Your Fans Will Love
+          </h1>
+          <p>
+            Huddle re-imagines the way we build communities. You have a voice,
+            but so does your audience. Create connections with your users as you
+            engage in genune discussion.
+          </p>
+          <button>Register</button>
+        </div>
+      </div>
+      <div className={styles.socials}>
+        <img src={fb} alt="facebook"></img>
+        <img src={twitter} alt="twitter"></img>
+        <img src={pinterest} alt="pinterest"></img>
+      </div>
     </div>
   );
 };
