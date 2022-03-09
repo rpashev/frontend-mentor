fe916ffe9f430e36a78f4e50ffe39420dad6f18b
rpashev <rossen1991@gmail.com>
2022-03-09 12:25:07 +0200

    single price grid component done

--- src/pages/single-price-grid/index.js
+++ src/pages/single-price-grid/index.js
@@ -3,7 +3,39 @@ import styles from "./index.module.scss";
 const SinglePriceGrid = () => {
   return (
     <div className={styles.page}>
-      <div className={styles.card}>HI</div>
+      <div className={styles.card}>
+        <div className={styles.heading}>
+          <h2>Join our community</h2>
+          <h3>30-day, hassle-free money back guarantee</h3>
+          <p>
+            Gain access to our full library of tutorials along with expert code
+            reviews.<br></br> Perfect for any developers who are serious about honing
+            their skills.
+          </p>
+        </div>
+        <div className={styles.content}>
+          <div className={styles.pricing}>
+            <h3>Monthly Subscription</h3>
+            <h4>
+              <span>$29</span>per month
+            </h4>
+            <p>Full access for less than $1 a day</p>
+            <button>Sign Up</button>
+          </div>
+          <div className={styles.features}>
+            <h3>Why Us</h3>
+            <ul>
+              <li>Tutorials by industry experts</li>
+              <li>Peer & expert code review</li>
+              <li>Coding exercises</li>
+              <li>Access to our GitHub repos</li>
+              <li>Community forum</li>
+              <li>Flashcard decks</li>
+              <li>New videos every week</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
