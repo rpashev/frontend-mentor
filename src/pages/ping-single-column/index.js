ee23f0e9a6c4fcb9b6f81971dd3dcc5642563d68
rpashev <rossen1991@gmail.com>
2022-03-09 13:48:18 +0200

    responsive ping component

--- src/pages/ping-single-column/index.js
+++ src/pages/ping-single-column/index.js
@@ -1,9 +1,32 @@
 import styles from "./index.module.scss";
+import img from "../../assets/ping-coming-soon-page-master/images/illustration-dashboard.png";
+import fb from "../../assets/ping-coming-soon-page-master/images/SVG/icon-facebook.svg";
+import insta from "../../assets/ping-coming-soon-page-master/images/SVG/icon-twitter.svg";
+import twitter from "../../assets/ping-coming-soon-page-master/images/SVG/icon-pinterest.svg";
 
 const PingSingleColumn = () => {
   return (
     <div className={styles.page}>
-      <div className={styles.card}>HI</div>
+      <div className={styles.card}>
+        <div className={styles.logo}>PING.</div>
+        <h1>
+          We are launching <span>soon!</span>
+        </h1>
+        <h5>Subscribe and get notified</h5>
+        <form>
+          <input type="email" placeholder="Your email address..." required></input>
+          <button>Notify Me</button>
+        </form>
+        <img className={styles.dashboard} src={img} alt="dashboard"></img>
+        <div className={styles.footer}>
+          <div className={styles.socials}>
+            <img src={fb} alt="facebook"></img>
+            <img src={twitter} alt="twitter"></img>
+            <img src={insta} alt="instagram"></img>
+          </div>
+          <p>&copy; Copyright Ping. All rights reserved</p>
+        </div>
+      </div>
     </div>
   );
 };
