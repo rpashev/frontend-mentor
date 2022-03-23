84f7c54d3f85c887cceef204b7b48054d2662cdf
rpashev <rossen1991@gmail.com>
2022-03-23 11:47:08 +0200

    pricing with toggle component done

--- src/pages/pricing-toggle/index.js
+++ src/pages/pricing-toggle/index.js
@@ -1,9 +1,67 @@
+import { useState } from "react";
 import styles from "./index.module.scss";
 
 const PricingToggleComponent = () => {
+  const [annually, setMonthly] = useState(true);
+
+  const toggleHandler = () => {
+    setMonthly((prevState) => !prevState);
+  };
+
   return (
     <div className={styles.page}>
-      <div className={styles.card}></div>
+      <section className={styles.section}>
+        <h1>Our Pricing</h1>
+        <div className={styles.toggle}>
+          <span>Annually</span>
+          <label className={styles.switch}>
+            <input type="checkbox" onChange={toggleHandler} />
+            <span className={styles.slider}></span>
+          </label>
+          <span>Monthly</span>
+        </div>
+        <div className={styles.cards}>
+          <div className={styles.card}>
+            <h3>Basic</h3>
+            <p className={styles.price}>
+              <span>$</span>
+              {!annually ? 19.99 : 199.99}
+            </p>
+            <ul>
+              <li>500 GB Storage</li>
+              <li>2 Users Allowed</li>
+              <li>Send up to 3GB</li>
+            </ul>
+            <button>Learn More</button>
+          </div>
+          <div className={`${styles.card} ${styles.premium}`}>
+            <h3>Professional</h3>
+            <p className={styles.price}>
+              <span>$</span>
+              {!annually ? 24.99 : 249.99}
+            </p>
+            <ul>
+              <li>1 TB Storage</li>
+              <li>5 Users Allowed</li>
+              <li>Send up to 10GB</li>
+            </ul>
+            <button>Learn More</button>
+          </div>
+          <div className={styles.card}>
+            <h3>Master</h3>
+            <p className={styles.price}>
+              <span>$</span>
+              {!annually ? 39.99 : 399.99}
+            </p>
+            <ul>
+              <li>2 TB Storage</li>
+              <li>10 Users Allowed</li>
+              <li>Send up to 20GB</li>
+            </ul>
+            <button>Learn More</button>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
